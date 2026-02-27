fbda7b16ef71f8ec69646349b2894162e44c723d
Aaradhya Maharishi <dadda2574@gmail.com>
2026-02-27 23:34:26 +0530

    Ini commit

---
+++ day3/script.js
@@ -0,0 +1,10 @@
+function changeText() {
+    document.getElementById("text").innerText = "Text Changed!";
+}
+
+let c = 0;
+
+function increase() {
+    c++;
+    document.getElementById("count").innerText = c;
+}
